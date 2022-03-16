25e6371d9600a817189e2f71c723aa37c0f6da8a
Ilhomjon Abdusaminov <abdusaminovilhomjon30037007@gmail.com>
2022-03-16 11:16:41 +0500

    Add project files.

---
+++ SifatSavdo.Domain/Customs/Constants.cs
@@ -0,0 +1,8 @@
+﻿
+namespace SifatSavdo.Domain.Customs
+{
+    public class Constants
+    {
+        public static readonly string CONNECTION_STRING = "Service=(localdb)\\MSSQLLocalDB; Database=SifatSavdoDB; Trusted_Connection=true;";
+    }
+}
