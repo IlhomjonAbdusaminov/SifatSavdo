dd44c8cffe59632249c22bc8fe2d627b380d6bc4
Ilhomjon Abdusaminov <abdusaminovilhomjon30037007@gmail.com>
2022-03-17 12:35:26 +0500

    Delete unnecessary libraries

--- SifatSavdo.Service/Services/ClientService.cs
+++ SifatSavdo.Service/Services/ClientService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SifatSavdo.Service.Services
+﻿namespace SifatSavdo.Service.Services
 {
     public class ClientService
     {
